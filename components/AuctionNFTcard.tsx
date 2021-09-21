e9a6c71396568a0d5d23a50f946e3935701f999e
PSOrigins <serapinaspijus@gmail.com>
2021-09-21 20:20:23 +0300

    Added auction house nft card

--- components/AuctionNFTcard.tsx
+++ components/AuctionNFTcard.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { setColors } from '../constants';
-import { Countdown } from './Countdown';
+
+import Countdown from 'react-countdown';
 
 import { GiTakeMyMoney } from 'react-icons/gi';
 
@@ -9,6 +10,8 @@ type AuctionCardProps = {
 };
 
 export const AuctionNFTcard = ({ nft }: AuctionCardProps) => {
+  const [showInfo, setShowInfo] = useState(false);
+
   useEffect(() => {
     fetch(`https://ipfs.io/ipfs/${nft.script}?${nft.uid}`)
       .then((response) => response.text())
@@ -20,22 +23,42 @@ export const AuctionNFTcard = ({ nft }: AuctionCardProps) => {
         )!.innerHTML = SVG;
       });
   }, [nft]);
+
   return (
     <div className="border shadow-xl h-auto border-transparent bg-gray-700 rounded-xl  text-white flex flex-col">
       <h1
         className="text-center w-full rounded-t-xl font-black py-2 text-xl"
         style={{ backgroundColor: setColors[nft.set] }}
       >
-        <Countdown deadline={nft.time} />
+        <Countdown date={Date.parse(nft.time)} />
       </h1>
       {/* {svg && <Image src={svg} alt="image" width={35} height={35} />} */}
-      <div className="py-5">
+      <div onClick={() => setShowInfo(true)} className="py-5 text-center">
         <div
           id={`image-${nft.set}-${nft.uid}-auction`}
           className="w-1/2 flex justify-center mx-auto"
         ></div>
+        <h1 className="pt-2 text-xl">
+          {nft.set}:{nft.uid}
+        </h1>
       </div>
-      <div className="px-2 py-4 w-full flex justify-center">
+      <div className="px-2 sm:px-4 py-4 w-full flex justify-between items-center gap-3">
+        <div>
+          <h1>
+            Price:{' '}
+            <strong>
+              {parseFloat(
+                (
+                  nft.price.amount / Math.pow(10, nft.price.precision)
+                ).toString()
+              ).toFixed(nft.price.precision)}{' '}
+              {nft.price.token}
+            </strong>
+          </h1>
+          <h1>
+            Bids: <strong>{nft.bids}</strong>
+          </h1>
+        </div>
         <button
           className="px-6 py-2 rounded-xl flex items-center gap-2"
           style={{ backgroundColor: setColors[nft.set] }}
--- pages/auction.tsx
+++ pages/auction.tsx
@@ -3,21 +3,30 @@ import React, { useEffect, useState } from 'react';
 import { AuctionNFTcard } from '../components';
 
 const Tools = () => {
-  const [auctionHouseNFT, setAuctionHouseNFT] = useState([]);
+  const [auctionHouseNFT, setAuctionHouseNFT] = useState<any>([]);
 
   useEffect(() => {
     fetch('https://token.dlux.io/api/auctions')
       .then((response) => response.json())
-      .then((data) => setAuctionHouseNFT(data.result));
+      .then((data) => {
+        setAuctionHouseNFT(data.result);
+      });
   }, []);
+
+  useEffect(() => {
+    console.log(auctionHouseNFT);
+  }, [auctionHouseNFT]);
 
   return (
     <div className="mx-2 sm:mx-10">
-      <h1 className="text-white my-10 text-2xl">Auction house</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-5 gap-5">
-        {auctionHouseNFT?.map((nft: any) => (
-          <AuctionNFTcard key={`${nft.set}-${nft.uid}`} nft={nft} />
-        ))}
+      <h1 className="text-white mt-10 mb-5 text-3xl font-bold">
+        Auction house
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-4 gap-5">
+        {auctionHouseNFT.length &&
+          auctionHouseNFT.map((nft: any) => (
+            <AuctionNFTcard key={nft.uid} nft={nft} />
+          ))}
       </div>
     </div>
   );
