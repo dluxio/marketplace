5982065b073dafcd2b51f430e749c5b22bef1ef0
PSOrigins <serapinaspijus@gmail.com>
2021-09-24 19:14:15 +0300

    Added form input link and nearly finished form

---
+++ components/FormInput.tsx
@@ -0,0 +1,36 @@
+import React, { ChangeEventHandler } from 'react';
+
+type FormInputProps = {
+  handleChange: ChangeEventHandler;
+  handleBlur: ChangeEventHandler;
+  value: any;
+  errors: any;
+  touched: any;
+  name: string;
+};
+
+export const FormInput = ({
+  handleChange,
+  handleBlur,
+  value,
+  errors,
+  touched,
+  name,
+}: FormInputProps) => {
+  return (
+    <div className="flex flex-col w-full">
+      <label className="text-left mb-1 text-md" htmlFor="start">
+        {name.charAt(0).toUpperCase() + name.slice(1)}
+      </label>
+      <input
+        className="px-3 py-1 rounded-lg border bg-gray-500 border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
+        type="text"
+        name={name}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={value}
+      />
+      <h1 className="text-red-500">{errors && touched && errors}</h1>
+    </div>
+  );
+};
--- pages/create-nft.tsx
+++ pages/create-nft.tsx
@@ -1,12 +1,128 @@
 import React from 'react';
 
+import { Formik } from 'formik';
+import { FormInput } from '../components/FormInput';
+
 const CrtateNFT = () => {
   return (
     <div className="overflow-hidden">
       <div className="flex flex-grow flex-1 justify-center items-center mt-20">
         <div className="border border-gray-800 bg-gray-600  text-white w-full sm:w-1/2 text-center rounded-xl">
           <h1 className="text-3xl py-4">Create an NFT</h1>
-          <form></form>
+          <Formik
+            initialValues={{
+              name: '',
+              type: 1 || 2,
+              script: '',
+              permlink: '',
+              start: '',
+              end: '',
+              royalty: '',
+              handling: 'svg',
+              max_fee: 1,
+              bond: 1,
+            }}
+            validate={({ name, script, permlink, start, end, royalty }) => {
+              const errors = {
+                name: '',
+                script: '',
+                permlink: '',
+                start: '',
+                end: '',
+                royalty: '',
+              };
+              if (!name) {
+                errors.name = 'Required';
+              } else if (!script) {
+                errors.script = 'Required';
+              } else if (!permlink) {
+                errors.permlink = 'Required';
+              } else if (!start) {
+                errors.start = 'Required';
+              } else if (!end) {
+                errors.end = 'Required';
+              } else if (!royalty) {
+                errors.royalty = 'Required';
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                console.log(values);
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <div className="w-full my-6">
+                  <div className="flex gap-5 mx-10 my-2">
+                    <FormInput
+                      name="name"
+                      value={values.name}
+                      errors={errors.name}
+                      handleBlur={handleBlur}
+                      handleChange={handleChange}
+                      touched={touched.name}
+                    />
+                    <FormInput
+                      name="permlink"
+                      value={values.permlink}
+                      handleBlur={handleBlur}
+                      handleChange={handleChange}
+                      errors={errors.permlink}
+                      touched={touched.permlink}
+                    />
+                  </div>
+                  <div className="mx-10">
+                    <FormInput
+                      name="script"
+                      handleBlur={handleBlur}
+                      handleChange={handleChange}
+                      value={values.script}
+                      errors={errors.script}
+                      touched={touched.script}
+                    />
+                  </div>
+                  <div className="flex gap-5 mx-10 my-2">
+                    <FormInput
+                      name="start"
+                      value={values.start}
+                      errors={errors.start}
+                      handleBlur={handleBlur}
+                      handleChange={handleChange}
+                      touched={touched.start}
+                    />
+                    <FormInput
+                      name="end"
+                      errors={errors.end}
+                      handleBlur={handleBlur}
+                      handleChange={handleChange}
+                      touched={touched.end}
+                      value={values.end}
+                    />
+                  </div>
+                  <div className="flex gap-5 mx-10 my-2">
+                    <FormInput
+                      name="royalty"
+                      value={values.royalty}
+                      errors={errors.royalty}
+                      handleBlur={handleBlur}
+                      handleChange={handleChange}
+                      touched={touched.royalty}
+                    />
+                  </div>
+                </div>
+              </form>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
