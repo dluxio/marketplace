a3569c29563c5a9bbf632aaf7d677c6b77bfc2bc
PSOrigins <serapinaspijus@gmail.com>
2021-09-30 14:17:41 +0300

    Changed next config

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
   images: {
     domains: ['assets.coingecko.com', 'ipfs.io'],
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    return config;
+  },
 };
