202049a0e519d984be2e5f1b5f0817f6dc3263ba
PSOrigins <serapinaspijus@gmail.com>
2021-10-20 18:47:29 +0300

    Finished trade card, added switch on trades route

--- components/TradeCard.tsx
+++ components/TradeCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
 type TradeCardProps = {
   trade: {
@@ -14,12 +15,36 @@ type TradeCardProps = {
 };
 
 export const TradeCard = ({ trade }: TradeCardProps) => {
+  useEffect(() => {
+    axios
+      .get(`https://ipfs.io/ipfs/${trade.script}?${trade.uid}`)
+      .then(({ data }) => {
+        const code = `(//${data}\n)("${trade.uid}")`;
+        const SVG = eval(code);
+        document.getElementById(
+          `${trade.item}-${trade.from}-${trade.to}`
+        )!.innerHTML = SVG.HTML;
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="rounded-xl border shadow-xl h-auto border-transparent bg-gray-700 text-white flex flex-col">
       <div className="text-center text-xl py-2 rounded-t-xl bg-black">
         <h1>{trade.from}</h1>
       </div>
-      <div className="flex sm:justify-evenly py-2">
+      <div className="w-full flex justify-center py-2">
+        <div
+          id={`${trade.item}-${trade.from}-${trade.to}`}
+          className="w-52"
+        ></div>
+      </div>
+      <div className="text-center text-xl pb-2">{trade.item}</div>
+      <div className="text-center text-md">
+        Price: {(trade.price / 1000).toFixed(3)} DLUX
+      </div>
+      <div className="flex sm:justify-evenly pt-2 pb-4">
         <button className="px-3 py-1 rounded-lg border-2 text-white bg-green-500 border-green-600 focus:outline-none focus:ring-2 focus:ring-green-700">
           Accept
         </button>
--- pages/trades.tsx
+++ pages/trades.tsx
@@ -4,11 +4,12 @@ import { userState } from '../atoms';
 
 import { useRouter } from 'next/router';
 import axios from 'axios';
-import { NftCard } from '../components';
 import { TradeCard } from '../components/TradeCard';
 
 const Trades = () => {
-  const [nftTrades, setNftTrades] = useState<[]>();
+  const [tradesToRespond, setTradesToRespond] = useState<[]>();
+  const [tradesToManage, setTradesToManage] = useState<[]>();
+  const [respond, setRespond] = useState(true);
   const router = useRouter();
   const user: any = useRecoilValue(userState);
 
@@ -19,8 +20,12 @@ const Trades = () => {
       axios
         .get(`https://token.dlux.io/api/trades/nfts/${user.name}`)
         .then(({ data }) => {
-          console.log(data);
-          setNftTrades(data.result);
+          setTradesToRespond(
+            data.result.filter((trade: any) => trade.from !== user.name)
+          );
+          setTradesToManage(
+            data.result.filter((trade: any) => trade.from === user.name)
+          );
         });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -29,12 +34,35 @@ const Trades = () => {
   return (
     <div className="mx-10 my-4 text-white font-medium">
       <title>Trades</title>
-      <h1 className="text-3xl">NFT Trades</h1>
+      <div className="flex justify-center">
+        <button
+          onClick={() => setRespond(true)}
+          className={`p-2 ${
+            respond && 'bg-gray-800'
+          } rounded-l-full bg-gray-700 text-white transition-all`}
+        >
+          Respond
+        </button>
+        <button
+          onClick={() => setRespond(false)}
+          className={`p-2 ${
+            !respond && 'bg-gray-800'
+          } rounded-r-full bg-gray-700 text-white transition-all`}
+        >
+          Manage
+        </button>
+      </div>
+      <h1 className="text-3xl">Trades</h1>
       <div className="grid grid-cols-1 sm:grid-cols-4 xl:grid-cols-5 gap-8 my-5">
-        {nftTrades &&
-          nftTrades.map((trade: any) => (
-            <TradeCard key={trade.item} trade={trade} />
-          ))}
+        {respond
+          ? tradesToRespond &&
+            tradesToRespond.map((trade: any) => (
+              <TradeCard key={trade.item} trade={trade} />
+            ))
+          : tradesToManage &&
+            tradesToManage.map((trade: any) => (
+              <h1 key={trade.item}>{trade.to}</h1>
+            ))}
       </div>
     </div>
   );
