e475e0efe0e8ccd00dcefd01454b3c649f2fe437
PijusSerapinasMipsas <pijus.s@mipsas.com>
2021-12-31 23:33:40 +0200

    Added masonary grid on profile page, changed the post styling

--- components/Card/PostCard.tsx
+++ components/Card/PostCard.tsx
@@ -1,28 +1,34 @@
-import React from "react";
-import SimpleImageSlider from "react-simple-image-slider/dist/ImageSlider";
+import React, { useEffect } from "react";
 import ReactJWPlayer from "react-jw-player";
+import { RiHeartFill } from "react-icons/ri";
+import { useRouter } from "next/router";
+import { MdComment } from "react-icons/md";
 
 type IPost = {
   title: string;
-  body: string;
-  images?: { url: string }[];
+  images?: string[];
   votes: any[];
   playlist?: any[];
   speak: boolean;
   author: any;
+  permlink: string;
+  pfp: string;
 };
 
 export const PostCard = ({
   title,
-  body,
   images,
   votes,
   speak,
   playlist,
   author,
+  permlink,
+  pfp,
 }: IPost) => {
+  const router = useRouter();
+
   return (
-    <div className="border border-gray-800 rounded-xl w-full bg-gray-600">
+    <div className="border-2 my-3 border-gray-800 rounded-xl w-full bg-gray-600">
       <div className="rounded-t-xl">
         {speak ? (
           <div className="w-full flex justify-center">
@@ -33,25 +39,43 @@ export const PostCard = ({
               playlist={playlist}
             />
           </div>
-        ) : images && images.length === 1 ? (
-          <img
-            className="w-full h-full rounded-t-xl"
-            src={images[0].url}
-            alt="thumbnail"
-          />
         ) : (
-          <SimpleImageSlider
-            height="100%"
-            width="100%"
-            style={{
-              borderTopLeftRadius: "0.75rem",
-              borderTopRightRadius: "0.75rem",
-            }}
-            images={images!}
-            showBullets={false}
-            showNavs={true}
-          />
+          images &&
+          images.length && (
+            <img
+              className="w-full h-full rounded-t-xl border-b-2 border-gray-800"
+              src={images[0]}
+              alt="thumbnail"
+            />
+          )
         )}
+      </div>
+      <div className="pt-2 px-5">
+        <div className="flex items-center gap-2">
+          <img
+            src={pfp}
+            className="rounded-full"
+            height={30}
+            width={30}
+            alt="profile_picture"
+          />
+          <h1>{author}</h1>
+        </div>
+        <div className="flex justify-center">{title}</div>
+        <div className="p-2 flex justify-center gap-2">
+          <button
+            onClick={() => router.replace(`/${author}/${permlink}`)}
+            className="flex gap-1 items-center bg-red-500 hover:bg-red-600 transition-all p-1 px-2 rounded-xl"
+          >
+            <RiHeartFill /> {votes ? votes.length : 0}
+          </button>
+          <button
+            onClick={() => router.replace(`/${author}/${permlink}`)}
+            className="flex gap-1 justify-center px-5 items-center bg-blue-500 hover:bg-blue-600 transition-all p-1 rounded-xl"
+          >
+            <MdComment />
+          </button>
+        </div>
       </div>
     </div>
   );
--- pages/[author].tsx
+++ pages/[author].tsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
+import Masonary from "react-masonry-css";
+import { isMobile } from "react-device-detect";
 import { useRouter } from "next/router";
-import { ceramicApi, hiveApi } from "../constants";
-import { Client, Discussion } from "@hiveio/dhive";
-import { NewsPost } from "../components/NewsPost";
+import { ceramicApi, hiveApi, placeHolder } from "../constants";
+import { Client } from "@hiveio/dhive";
 import { useTranslation } from "next-export-i18n";
 import { useHiveKeychainCeramic } from "spk-auth-react";
 import { getUserPosts } from "../utils";
 import { ProfileCard } from "../components/Card/ProfileCard";
+import { PostCard } from "../components/Card/PostCard";
 
 const User = () => {
   const client = new Client(hiveApi);
@@ -14,18 +16,24 @@ const User = () => {
   const router = useRouter();
   const { author } = router.query;
   const [username, setUsername] = useState({ username: "", provider: "" });
-  const [posts, setPosts] = useState<Discussion[]>([]);
+  const [posts, setPosts] = useState<any[]>([]);
   const [userData, setUserData] = useState<any>(null);
   const { t } = useTranslation();
 
   useEffect(() => {
-    if (author && (author! as string).slice(0, 1) === "@") {
-      setUsername({
-        username: (author! as string).slice(1, author!.length),
-        provider: "hive",
-      });
-    } else {
-      setUsername({ username: author as string, provider: "ceramic" });
+    if (author) {
+      const firstChar = (author! as string).slice(0, 1);
+
+      if (firstChar === "@") {
+        setUsername({
+          username: (author! as string).slice(1, author!.length),
+          provider: "hive",
+        });
+      } else if (firstChar === "d") {
+        setUsername({ username: author as string, provider: "ceramic" });
+      } else {
+        router.push("/404");
+      }
     }
   }, [author]);
 
@@ -33,7 +41,7 @@ const User = () => {
     const getCeramicProfile = async (didId: string) => {
       const response = await connector.idx.get("basicProfile", didId);
       return response;
-    } 
+    };
 
     if (username) {
       if (username.provider === "hive") {
@@ -58,20 +66,24 @@ const User = () => {
       } else if (username.username) {
         getCeramicProfile(username.username).then((profile: any) => {
           if (profile) {
-            setUserData(profile.profile ? profile.profile : {
-              about: profile.description || null,
-              name: profile.name || null,
-              profile_image: profile.image || null,
-              cover_image: profile.background || null,
-              website: profile.url || null,
-              location: profile.homeLocation || null,
-            })
+            setUserData(
+              profile.profile
+                ? profile.profile
+                : {
+                    about: profile.description || null,
+                    name: profile.name || null,
+                    profile_image: profile.image || null,
+                    cover_image: profile.background || null,
+                    website: profile.url || null,
+                    location: profile.homeLocation || null,
+                  }
+            );
           }
         });
 
-        getUserPosts(username.username).then(response => {
-          console.log(response);
-        })
+        getUserPosts(username.username).then((response) => {
+          setPosts(response);
+        });
       }
     }
   }, [username]);
@@ -85,12 +97,35 @@ const User = () => {
       />
       <div className="text-center">
         <h1 className="my-3 font-bold text-xl">{t("recent")}</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 w-full my-4">
+        <Masonary
+          breakpointCols={isMobile ? 1 : 2}
+          className="masonry-grid sm:mx-4"
+          columnClassName="masonry-grid_column"
+        >
           {posts.map((post) => {
+            const metadata = post.json_metadata
+              ? JSON.parse(post.json_metadata)
+              : null;
             console.log(post);
-            return <NewsPost key={post.permlink} post={post} />;
+            return post.active_votes ? (
+              <PostCard
+                key={post.permlink}
+                author={post.author}
+                permlink={post.permlink}
+                speak={false}
+                title={post.title}
+                votes={post.active_votes}
+                images={metadata.image}
+                playlist={[]}
+                pfp={
+                  userData?.profile_image ? userData.profile_image : placeHolder
+                }
+              />
+            ) : (
+              <h1>Something</h1>
+            );
           })}
-        </div>
+        </Masonary>
       </div>
     </div>
   );
