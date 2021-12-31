85d3c3656c207b4cfb51f4d8c17f1543ef68a6ff
PijusSerapinasMipsas <pijus.s@mipsas.com>
2021-12-31 19:50:37 +0200

    Responsive image slider

--- components/Card/PostCard.tsx
+++ components/Card/PostCard.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import SimpleImageSlider from 'react-simple-image-slider/dist/ImageSlider'
+import React from "react";
+import SimpleImageSlider from "react-simple-image-slider/dist/ImageSlider";
 import ReactJWPlayer from "react-jw-player";
 
 type IPost = {
-  title: string,
-  body: string,
-  images: { url: string }[],
-  votes: any[],
-  playlist?: any[],
-  speak: boolean,
-  author: any
-}
+  title: string;
+  body: string;
+  images: { url: string }[];
+  votes: any[];
+  playlist?: any[];
+  speak: boolean;
+  author: any;
+};
 
-export const PostCard = ({ title, body, images, votes, speak, playlist, author }: IPost) => {
-
+export const PostCard = ({
+  title,
+  body,
+  images,
+  votes,
+  speak,
+  playlist,
+  author,
+}: IPost) => {
   return (
     <div className="border border-gray-800 rounded-xl w-full bg-gray-600">
       <div className="rounded-t-xl">
@@ -28,15 +35,14 @@ export const PostCard = ({ title, body, images, votes, speak, playlist, author }
           </div>
         ) : (
           <SimpleImageSlider
-            height={500}
-            width={1000}
-            images={images}
-            style={{ width: '100%', height: '100%' }}
-            showBullets={false}
-            showNavs={true}
+              height="100%"
+              width="100%"
+              images={images}
+              showBullets={false}
+              showNavs={true}
           />
         )}
       </div>
     </div>
-  )
-}
+  );
+};
