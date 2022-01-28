743b5b8ebedee4812a0fa15c75d0bff4db2cbdd9
PijusSerapinasMipsas <pijus.s@mipsas.com>
2022-01-28 08:38:35 +0200

    new build

---
+++ out/_next/static/chunks/0c428ae2-253f127c26b8b9a252fa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[13],{63750:function(t,n,r){r.d(n,{HHm:function(){return a}});var u=r(44405);function a(t){return(0,u.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"}}]})(t)}}}]);
